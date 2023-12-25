71b3536e0144ba4c03f628858868c89e7351b57c
poseisharp <poseiayayui@gmail.com>
2023-12-26 00:23:58 +0700

    [Level] outline

---
+++ levels/level.gd
@@ -0,0 +1,58 @@
+# Extend node type
+# Must the same type with the node which the script attache into
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+  print("==Level started==")
+  print("Waiting for player to place their first seed")
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+  pass
+
+# TODO 
+# Soil, if clicked it will become loose and can reroute the water
+
+# If seed collect water increase experience
+
+# When level up pick 1 out of 3 power up
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
