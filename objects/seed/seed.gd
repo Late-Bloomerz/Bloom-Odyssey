cadf3e6237ea7e64a97e962f3b15c5642c2c5013
poseisharp <poseiayayui@gmail.com>
2023-12-28 01:52:39 +0700

    [App] idk

--- objects/seed/seed.gd
+++ objects/seed/seed.gd
@@ -1,16 +1,26 @@
 extends Node2D
+class_name Seed
+
+@export var hp: int = 100
+@export var max_hp: int = 100
+@export var experience_gain: int = 10
+
+@onready var sprite: Sprite2D = $Sprite
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-  pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
-  pass
-
+func is_seed() -> bool:
+  return true
 
 func _on_experience_timer_timeout():
-  GameSignal.emit_signal("experience_generated", 10)
-  pass # Replace with function body.
+  GameSignal.emit_signal("experience_generated", experience_gain)
+
+func _on_hurtbox_area_entered(area):
+  if area.has_method("is_enemy"):
+    hp -= 10
+    sprite.modulate = Color.RED
+    await get_tree().create_timer(0.1).timeout
+    sprite.modulate = Color.WHITE
+    if hp <=0:
+      remove_from_group("Seed")
+      queue_free()
+      GameSignal.emit_signal("seed_died", self)
